7a453d0b520998e7dc7071ad9e20c0cdce57ec32
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-11-25 02:11:43 -0500

    se elimino func no usada

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,6 @@ import hashlib
 from django.views.generic import ListView
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Sum
