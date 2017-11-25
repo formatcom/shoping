5d905c09269548d15d2ae1f5641e42368f5ad230
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-11-24 22:50:39 -0500

    se soluciono error con los decimales

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Item(models.Model):
 
     name = models.CharField(blank=False, max_length=150)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     cover = models.URLField()
 
 
